6cbec9cabf7e1e92709f0242c97de285a3e71bd3
Angel Monegro <amonegro@remotequeue.com>
2016-12-17 10:01:43 +0000

    Improved erro messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,18 +2,20 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
-def create
+
+ def create
   @contact = Contact.new(contact_params)
   if @contact.save
-    redirect_to new_contact_path, notice: "Message sent."
+     flash[:success] = "Message sent."
+     redirect_to new_contact_path
   else
-    redirect_to new_contact_path, notice: "Error occured."
+     flash[:error] = @contact.errors.full_messages.join(", ")
+     redirect_to new_contact_path
   end
-end
+ end
 
-  private
-    def contact_params
+private
+  def contact_params
      params.require(:contact).permit(:name, :email, :comments)
-    end
+  end
 end

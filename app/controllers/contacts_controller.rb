c786112dc40ed59f722b127aab8e2f82cadbd9b6
Angel Monegro <amonegro@remotequeue.com>
2016-12-18 18:55:14 +0000

    Added custom form parth

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,7 +13,7 @@ class ContactsController < ApplicationController
      redirect_to new_contact_path
   end
  end
-
+ 
 private
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)

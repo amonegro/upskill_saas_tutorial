210a49f206515e0c328a583309c5cb6f24a881e4
Angel Monegro <amonegro@remotequeue.com>
2016-12-22 03:18:08 +0000

    Edited error in  profiles table migration file

--- db/migrate/20161222025334_create_profiles.rb
+++ db/migrate/20161222025334_create_profiles.rb
@@ -7,7 +7,7 @@ class CreateProfiles < ActiveRecord::Migration[5.0]
       t.string :job_title
       t.string :phone_number
       t.string :contact_email
-      t.text: :description
+      t.text :description
       t.timestamps
     end
   end
